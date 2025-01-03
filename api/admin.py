37a5a85032428c903b7608c5332540a41f85b9a0
Ahmad Dzaky <dzakykun11@gmail.com>
2025-01-04 05:29:35 +0700

    inital upload

---
+++ api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import UserRating
+
+admin.site.register(UserRating)
+
+
---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relasi ke tabel user
+    id_movie = models.CharField(max_length=50)  
+    rating = models.FloatField()
+    genre = models.CharField(max_length=100, default="Unknown") 
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp otomatis
+
+    def __str__(self):
+        return f"{self.user.username} - Movie ID: {self.id_movie} - Rating: {self.rating}"
